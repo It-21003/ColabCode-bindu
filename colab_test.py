1ca74033309e5f9e0f1974a0e099d3e64c318754
It-21003 <157291494+It-21003@users.noreply.github.com>
2024-11-07 20:53:08 +0600

    Add files via upload

---
+++ colab_test.py
@@ -0,0 +1,8 @@
+# colab_test.py
+from BinduMath import printBigpi
+
+# Set the number of decimal places for pi
+n = 10  # Adjust this value as needed for your test
+
+# Print pi to n decimal places
+print("Pi to", n, "decimal places is:", printBigpi(n))
